5c8240c7c814a15defcae6eb5a24b6af0478adcf
Artiom Chizh <artemciz993@gmail.com>
2026-02-03 12:24:28 +0300

    add tests

---
+++ tests/test_fail.py
@@ -0,0 +1,11 @@
+def test7():
+    assert False
+def test8():
+    assert False
+def test9():
+    assert False
+def test10():
+    assert False
+def test11():
+    assert False
+
---
+++ tests/test_pass.py
@@ -0,0 +1,12 @@
+def test1():
+    assert True
+def test2():
+    assert True
+def test3():
+    assert True
+def test4():
+    assert True
+def test5():
+    assert True
+def test6():
+    assert True
